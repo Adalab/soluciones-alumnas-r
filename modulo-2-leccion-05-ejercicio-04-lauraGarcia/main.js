83d4fdbb2f344158b3b2ded93ce2153478703ce1
Laura Garcia <lgarciacuello@gmail.com>
2022-10-23 17:47:29 +0200

    add exercise

---
+++ modulo-2-leccion-05-ejercicio-04-lauraGarcia/main.js
@@ -0,0 +1,16 @@
+"user stric"
+
+function paresNones (a){
+    if(a % 2 === 0){  
+        return console.log(`El número es par`); 
+    }
+    else {
+        return console.log ('El número es impar');
+      }
+}
+
+paresNones (5);
+
+paresNones (10);
+
+paresNones (13)
