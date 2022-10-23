b4f41c5a8827d63ed86bad0f2e11d6866902995e
Yara <yara.gordillo@gmail.com>
2022-10-23 22:25:33 +0200

    changes on old exercises

--- modulo-2-leccion-01-ejercicio-04-adalaber-seleccionada-YaraPaz/main.js
+++ modulo-2-leccion-01-ejercicio-04-adalaber-seleccionada-YaraPaz/main.js
@@ -1,6 +1,8 @@
-"use strict";
+'use strict';
 
-const ChosenAdalaberMessage = document.querySelector(".title");
-const ChosenAdalaber = document.querySelector(".chosen1");
-ChosenAdalaberMessage.innerHTML =
-  ChosenAdalaberMessage.innerHTML + ":" + " " + ChosenAdalaber.innerHTML;
+const ChosenAdalaberMessage = document.querySelector('.title');
+// const ChosenAdalaber = document.querySelector(".chosen1");
+// ChosenAdalaberMessage.innerHTML =
+//   ChosenAdalaberMessage.innerHTML + ":" + " " + ChosenAdalaber.innerHTML;
+const chosenAdalaber = document.querySelector('.chosen1').innerHTML;
+ChosenAdalaberMessage.innerHTML += ':' + ' ' + chosenAdalaber;
