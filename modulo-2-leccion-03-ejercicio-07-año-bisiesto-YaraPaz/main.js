05087e1e55f770ca4cd38ae6077544e46f1c0d54
Yara <yara.gordillo@gmail.com>
2022-10-25 22:06:05 +0200

    refactorising leap year exercise

--- modulo-2-leccion-03-ejercicio-07-año-bisiesto-YaraPaz/main.js
+++ modulo-2-leccion-03-ejercicio-07-año-bisiesto-YaraPaz/main.js
@@ -2,7 +2,10 @@
 
 let currentYear = 2020;
 let yearCalc = currentYear % 4;
+let hundredException = currentYear % 100;
+let fourHundredException = currentYear % 400;
 
+//Switch option less complete
 switch (yearCalc) {
   case 0:
     console.log('El próximo año bisiesto será' + ' ' + (currentYear + 4));
@@ -19,4 +22,29 @@ switch (yearCalc) {
   case 4:
     console.log('Este año es bisiesto');
     break;
+}
+
+//If option more complete
+if ((yearCalc === 0 && hundredException !== 0) || fourHundredException === 0) {
+  console.log('El próximo año bisiesto será' + ' ' + (currentYear + 4));
+} else if (
+  (yearCalc === 1 && hundredException !== 1) ||
+  fourHundredException === 1
+) {
+  console.log('El próximo año bisiesto será' + ' ' + (currentYear + 3));
+} else if (
+  (yearCalc === 2 && hundredException !== 2) ||
+  fourHundredException === 2
+) {
+  console.log('El próximo año bisiesto será' + ' ' + (currentYear + 2));
+} else if (
+  (yearCalc === 3 && hundredException !== 3) ||
+  fourHundredException === 3
+) {
+  console.log('El próximo año bisiesto será' + ' ' + (currentYear + 1));
+} else if (
+  (yearCalc === 4 && hundredException !== 4) ||
+  fourHundredException === 4
+) {
+  console.log('El próximo año bisiesto será' + ' ' + (currentYear + 0));
 }
