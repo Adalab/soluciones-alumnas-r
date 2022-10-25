8f786bef1279577ee4fd3db6fd118236fb3c6a14
Bella Irene <bellagarcia1985@gmail.com>
2022-10-25 17:55:48 +0200

    commit

--- modulo-2-lleccion-03-ejercicio-02-control-de-acceso-YaraPaz/main.js
+++ modulo-2-lleccion-03-ejercicio-02-control-de-acceso-YaraPaz/main.js
@@ -1,13 +1,13 @@
 'use strict';
-
-let allowedUserName = '';
+let saludo = 'cosa';
+// let allowedUserName = 'Maria';
 let input = document.querySelector('#username');
-let inputValueAttribute = input.setAttribute('value', `${allowedUserName}`);
+// let inputValueAttribute = input.setAttribute('value', `${allowedUserName}`);
 let inputValueInfo = document.getElementById('username').value;
 let text = document.querySelector('.text');
 
 if (inputValueInfo === 'Maria') {
-  text.innerHTML = `Bienvenida, ${allowedUserName}`;
+  text.innerHTML = `Bienvenida, ${saludo}`;
 } else if (inputValueInfo === '') {
   text.innerHTML = 'Introduce un nombre de usuario';
 } else if (inputValueInfo !== 'Maria') {
